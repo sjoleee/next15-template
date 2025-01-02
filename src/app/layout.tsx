2ca201df2cc8e33ba1f01a3de83664ea09b99a0c
sjoleee <cent7425@gmail.com>
2025-01-02 23:38:11 +0900

    add tools

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
+
+import LazyMotionProvider from "@/components/LazyMotionProvider";
+import TanstackQueryClientProvider from "@/components/TanstackQueryClientProvider";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const pretendard = localFont({
+  src: "../../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
 });
 
 export const metadata: Metadata = {
@@ -23,12 +23,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ko">
+      <TanstackQueryClientProvider>
+        <LazyMotionProvider>
+          <body className={`${pretendard.variable} antialiased`}>{children}</body>
+        </LazyMotionProvider>
+      </TanstackQueryClientProvider>
     </html>
   );
 }
